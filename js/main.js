c8b9cac1ab56826473a878451606e526f3e0d117
Matt Michaels <matt.michaels@indigoslate.com>
2015-12-28 16:35:35 -0800

    progressing.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-
   // IE Version flagging
   if(Function('/*@cc_on return document.documentMode===9@*/')()){ $("html").addClass("ie9"); }
   if(Function('/*@cc_on return document.documentMode===10@*/')()){ $("html").addClass("ie10"); }
+
+
+
+  // SVG Class toggling
+
+  $('#infographic2--optionA').hover(function() {
+    console.log('hovered');
+  });
 });
