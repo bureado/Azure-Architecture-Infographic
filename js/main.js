a2c42291b3d0c521be1bf5702790879f06857dbf
Matt Michaels <matt.michaels@indigoslate.com>
2015-12-31 16:14:15 -0800

    testing js

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
   // Toggling SVG icons when clicked
   $('.question--infographic2 .icons').click(function() {
     $('.answer--infographic2').show();
-    $(this).toggleClass('active').siblings().removeClass('active');    
+    $(this).toggleClass('active').siblings().removeClass('active');
   });
 
 
@@ -23,55 +23,32 @@ $(document).ready(function(){
   $('.question--infographic2 .icons, .answer--nav ul li').click(function() {
     $('.answer--infographic2 .answer--details').hide();
 
+    // Scrolling answer container to top of screen    
+    $('html, body').animate({
+        scrollTop: $(".answer--infographic2").offset().top -0
+    }, 500);
+    
+
+
     if($(this).hasClass('icons-2A') || $(this).hasClass('answer--nav__2A'))  {
+      $('.icons-2A').addClass('active').siblings().removeClass('active');               // Showing corresponding active icon below answers
+      $('.answer--details-2A').show();                                                  // Showing corresponding answer panel
+      $('.answer--nav__2A').addClass('active-nav').siblings().removeClass('active-nav');  // Toggling corresponding answer side-nav
       
-      $('.answer--details-2A').show();
-      $('.active-nav').toggleClass('active-nav').siblings().removeClass('active-nav');    
-      $('.answer--nav__2A').addClass('active-nav');
     }
 
     if($(this).hasClass('icons-2B') || $(this).hasClass('answer--nav__2B')) {
-      
+      $('.icons-2B').addClass('active').siblings().removeClass('active'); 
       $('.answer--details-2B').show();
-      $('.active-nav').toggleClass('active-nav').siblings().removeClass('active-nav');    
-      $('.answer--nav__2B').addClass('active-nav');
+      $('.answer--nav__2B').addClass('active-nav').siblings().removeClass('active-nav');    
     }
 
     if($(this).hasClass('icons-2C') || $(this).hasClass('answer--nav__2C')) {
-
+      $('.icons-2C').addClass('active').siblings().removeClass('active'); 
       $('.answer--details-2C').show();
-      $('.active-nav').toggleClass('active-nav').siblings().removeClass('active-nav');    
-      $('.answer--nav__2C').addClass('active-nav');
+      $('.answer--nav__2C').addClass('active-nav').siblings().removeClass('active-nav');    
     }
   });
-
-  
-  // Toggling content via navigation
-//  $('.answer--nav ul li').click(function() {
-  //  $('.answer--infographic2 .answer--details').hide();
-
-    // if($(this).hasClass('answer--nav__2A')) { 
-    //   $('.answer--details-2A').show();
-    //   $('.active-nav').toggleClass('active-nav').siblings().removeClass('active-nav');
-    //   $('.answer--nav__2A').addClass('active-nav');
-      
-    // }
-
-    // if() {
-    //   $('.answer--details-2B').show();
-    //   $('.active-nav').toggleClass('active-nav').siblings().removeClass('active-nav');    
-    //   $('.answer--nav__2B').addClass('active-nav');
-    //   $('.icons-2B').addClass('active').siblings().removeClass('active');
-    // }
-
-    // if() {
-    //   $('.answer--details-2C').show();
-    //   $('.active-nav').toggleClass('active-nav').siblings().removeClass('active-nav');    
-    //   $('.answer--nav__2C').addClass('active-nav');
-    //   $('.icons-2C').addClass('active').siblings().removeClass('active');
-    // }
-  //});
-
 
 });
 
